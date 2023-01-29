c671842c5217fd05c6e8ab1919b2edec73aeb162
grze2000 <programista333@gmail.com>
2023-01-29 02:41:06 +0100

    update help

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -8,7 +8,11 @@ export const showHelp = async (msg: Message) => {
       `Użyj polecenia **${process.env.PREFIX}users** aby wyświetlić listę losowych użytkowników serwera.\n
         Możesz określić minimalną datę dołączenia użytkownika dopisując: **from <DD.MM.RRRR>**\n
         Możesz określić maksymalną datę dołączenia użytkownika dopisując: **to <DD.MM.RRRR>**\n
-        Możesz określić maksymalną ilość użytkowników dopisując: **max <liczba>**
+        Możesz określić maksymalną ilość użytkowników dopisując: **max <liczba>**\n
+        --------------------- Odliczanie ---------------------\n
+        **${process.env.PREFIX}odliczanie** - wyświetla listę ustawionych kanałów odliczania\n
+        **${process.env.PREFIX}odliczanie dodaj #nazwa-kanału** - włącza odliczanie na podanym kanale\n
+        **${process.env.PREFIX}odliczanie usuń #nazwa-kanału** - wyłącza odliczanie na podanym kanale
         `
     )
     .setFooter({
